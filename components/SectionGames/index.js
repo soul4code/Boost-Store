3995df41035da7526d48f93f1abb500c926ce2df
Trix <89504082116@mail.ru>
2021-10-11 16:39:07 +0700

    получение данные игр в начале проекта

--- components/SectionGames/index.js
+++ components/SectionGames/index.js
@@ -1,43 +1,31 @@
-import {connect} from "react-redux";
-import {setGames} from "../../store/games/actions";
-import {useEffect} from "react";
-import Link from 'next/link';
-import {api} from '../../utils/api/api';
+import { connect } from "react-redux";
+import Link from "next/link";
 import GameCard from "../GameCard";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import en from "../../locale/main/en";
 import ru from "../../locale/main/ru";
 
 const SectionGames = (props) => {
+  const router = useRouter();
+  const t = router.locale === "ru" ? ru : en;
 
-    const router = useRouter();
-    const t = router.locale === "ru" ? ru : en;
+  return (
+    <section className="section-games container">
+      <h2 className="section-games__title">{t.gamesSectionTitle}</h2>
+      <div className="section-games__catalog">
+        {props.games.map((game) => {
+          return (
+            <Link href={`/${game.CODE}`} key={game.ID} passHref>
+              <GameCard
+                picture={game.PICTURE}
+                link={`/${game.CODE}`}
+                alt={game.NAME}
+              />
+            </Link>
+          );
+        })}
 
-    useEffect(() => {
-        api.getGames()
-            .then(res => {
-                props.setGames(res);
-            })
-            .catch(err => {
-                console.log(`Ошибка при получении списка игр ${err}`);
-            })
-    }, [])
-
-    return (
-        <section className="section-games container">
-            <h2 className="section-games__title">{ t.gamesSectionTitle }</h2>
-            <div className="section-games__catalog">
-                {
-                    props.games.map(game => {
-                        return (
-                            <Link href={`/${game.CODE}`} key={game.ID} passHref>
-                                <GameCard picture={game.PICTURE} link={`/${game.CODE}`} alt={game.NAME}/>
-                            </Link>
-                        );
-                    })
-                }
-
-                {/* <a className="section-games__players" href='#'>
+        {/* <a className="section-games__players" href='#'>
                     <div className="section-games__players-icon">
                         <img src="img/icons/man.svg" alt="more then 500 professionals players"/>
                         <img className="section-games__players-hov" src="img/icons/man-blue.svg"
@@ -47,19 +35,14 @@ const SectionGames = (props) => {
                         More than <b>500 professionals players</b>
                     </div>
                 </a> */}
-            </div>
-        </section>
-    );
-}
+      </div>
+    </section>
+  );
+};
 
-const mapStateToProps = state => ({
-    games: state.games.games,
-    lang: state.lang.lang
-})
+const mapStateToProps = (state) => ({
+  games: state.games.games,
+  lang: state.lang.lang,
+});
 
-const mapDispatchToProps = {
-    setGames
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(SectionGames);
+export default connect(mapStateToProps)(SectionGames);
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,32 @@
-import '../css/nouislider.min.css'
-import '../css/custom-select.css'
-import '../css/swiper.min.css'
-import '../css/globals.css'
+import "../css/nouislider.min.css";
+import "../css/custom-select.css";
+import "../css/swiper.min.css";
+import "../css/globals.css";
+
+import { api } from "../utils/api/api";
 
 import { Provider } from "react-redux";
 import store from "../store/store";
 
+import { setGames } from "../store/games/actions";
+import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    api
+      .getGames()
+      .then((res) => {
+        store.dispatch(setGames(res));
+      })
+      .catch((err) => {
+        console.log(`Ошибка при получении списка игр ${err}`);
+      });
+  }, []);
 
   return (
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
   );
 }
 
